5af04460c6cc2498b624f7894ed8a455e4e3c580
Abed MAATALLA <abedmaatalla@gmail.com>
2016-11-24 17:36:03 +0100

    slim framework translation

---
+++ README.md
@@ -0,0 +1,86 @@
+# Slim Twig Translation Extension
+
+This repository provides a twig extension class for the twig view parser. 
+The class adds a translate helper function  for the use in twig templates.
+The translator function tries to call the trans() function of an 
+Illuminate\Translation\Translator object in the slim DI container. 
+
+## How to install
+
+#### using [Composer](http://getcomposer.org/)
+
+Create a composer.json file in your project root:
+    
+```json
+{
+    "require": {
+        "abedmaatalla/slim-twig-translation-extension": "v0.1.0"
+    }
+}
+```
+
+Then run the following composer command:
+
+```bash
+$ php composer.phar install
+```
+
+## How to use
+
+Create new folder for langauges 
+
++
+-- lang
+  + -- en
+      + -- file.php
+  + -- fr
+      + -- file.php
+
+### Tanslator 
+
+Set up your Tansloator 
+
+$container['translator'] = function ($c)
+{
+  // Register the English translator 'en'
+  $translator = new Illuminate\Translation\Translator(new Illuminate\Translation\FileLoader(new Illuminate\Filesystem\Filesystem(), __DIR__ . '/lang'), 'en');
+    // setLocal for new location 
+    $translator->setLocale('fr');
+    return $translator;
+};
+
+### Slim
+
+Set up your twig views as described in the [SlimViews Repository](https://github.com/codeguy/Slim-Views).
+Add the extension to your parser extensions.
+
+```php
+$container['view'] = function ($c)
+{
+  $view = new \Slim\Views\Twig('../resources/views');
+
+  // add translator functions to Twig
+  $view->addExtension(new \abedmaatalla\Slim\Twig\Extension\TranslationExtension($c->translator));
+
+}
+```
+
+### Twig template
+
+In your twig template you would write:
+
+```
+  {{ translate('male') }}
+```
+  
+You can also use the shorthand:
+
+```
+  {{ tans('male') }}
+```  
+
+You can also use the shorthand:
+
+```
+  {{ _('male') }}
+```
---
+++ src/TranslationExtension.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * TranslationExtension adds a translate function for twig templates in a slim framework application.
+ *
+ * TranslationExtension expects an instance of a translator class injected into the slim app and tries 
+ * to call the "trans()" function on the translator class.
+ *
+ * Example usage:
+ * {{ translate('male') }}
+ * {{ tans('male') }}
+ * {{ _('male') }}
+ * 
+ * @author Abed MAATALLA <abedmaatalla@gmail.com>
+ * @copyright 2014 Abed MAATALLA
+ * @package Pmt\Slim\Twig\Extension
+ * 
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+namespace Pmt\Slim\Twig\Extension;
+
+use Illuminate\Translation\Translator;
+
+/**
+ * Registers the Illuminate Translator as a the trans and transChoice functions in Twig
+ */
+class TranslationExtension extends \Twig_Extension
+{
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function getName()
+    {
+        return 'slim_translator';
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('translate', array($this->translator, 'trans')),
+            new \Twig_SimpleFunction('trans', array($this->translator, 'trans')),
+            new \Twig_SimpleFunction('_', array($this->translator, 'trans')),
+        ];
+    }
+}
+
