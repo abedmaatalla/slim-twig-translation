5268f6256b36518b51b16a74a23ea080c95a7c56
Thiago <thaigoandroid@gmail.com>
2018-01-17 18:01:18 -0200

    fixing problem on class namespace

--- src/TranslationExtension.php
+++ src/TranslationExtension.php
@@ -35,7 +35,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-namespace Pmt\Slim\Twig\Extension;
+namespace abedmaatalla\Slim\Twig\Extension;
 
 use Illuminate\Translation\Translator;
 
